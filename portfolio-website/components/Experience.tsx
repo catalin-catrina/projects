0682f02d4da7a021173c61010a393dc1fbec167f
Catalin Catrina <catalin.catrina@outlook.com>
2025-04-19 00:20:54 +0300

    sketched the experience section

--- portfolio-website/components/Experience.tsx
+++ portfolio-website/components/Experience.tsx
@@ -1,3 +1,55 @@
 export const Experience = () => {
-  return <></>;
+  return (
+    <section id="experience">
+      <div className="max-w-[1200px] mx-auto px-4 md:px-8 text-center">
+        <h2 className="inline-block text-6xl bg-linear-to-r from bg-fuchsia-600 to-sky-500 bg-clip-text text-transparent">
+          Experience
+        </h2>
+        <div className="flex flex-col gap-10">
+          <div className="text-gray-200 text-left w-1/2">
+            <h3>Frontend Developer</h3>
+            <p>Cognizant Softvision</p>
+            <p>Aug 2022 - Feb 2025</p>
+            <p>
+              Tech: HTML, CSS / SCSS, JavaScript, TypeScript, Angular, React
+              Worked in an Agile (Kanban) environment to maintain and improve an
+              established healthcare provider application using Angular.
+              Debugged and resolve complex issues within the existing codebase,
+              enhancing application stability and user experience. Implemented
+              design updates and UI refinements to improve the
+              application&apos;s look and feel while maintaining consistency.
+            </p>
+          </div>
+          <div className="text-gray-200 text-left w-1/2 self-end">
+            <h3>Frontend Developer</h3>
+            <p>Cognizant Softvision</p>
+            <p>Aug 2022 - Feb 2025</p>
+            <p>
+              Tech: HTML, CSS / SCSS, JavaScript, TypeScript, Angular, React
+              Worked in an Agile (Kanban) environment to maintain and improve an
+              established healthcare provider application using Angular.
+              Debugged and resolve complex issues within the existing codebase,
+              enhancing application stability and user experience. Implemented
+              design updates and UI refinements to improve the
+              application&apos;s look and feel while maintaining consistency.
+            </p>
+          </div>
+          <div className="text-gray-200 text-left w-1/2">
+            <h3>Frontend Developer</h3>
+            <p>Cognizant Softvision</p>
+            <p>Aug 2022 - Feb 2025</p>
+            <p>
+              Tech: HTML, CSS / SCSS, JavaScript, TypeScript, Angular, React
+              Worked in an Agile (Kanban) environment to maintain and improve an
+              established healthcare provider application using Angular.
+              Debugged and resolve complex issues within the existing codebase,
+              enhancing application stability and user experience. Implemented
+              design updates and UI refinements to improve the
+              application&apos;s look and feel while maintaining consistency.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
